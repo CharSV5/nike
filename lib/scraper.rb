9363ecd358648deb2da149482d9528cb2d1013cb
CharSV5 <charlenebastians@hotmail.com>
2018-05-20 10:45:38 +0100

    initialize method to get and parse

---
+++ lib/scraper.rb
@@ -0,0 +1,11 @@
+require 'HTTParty'
+require 'Nokogiri'
+
+class Scraper
+  attr_accessor :parse_page
+
+  def initialize
+    doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3")
+    @parse_page ||= Nokogiri::HTML(doc)
+  end
+end
